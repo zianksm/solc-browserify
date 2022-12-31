5d27be9a8236d8f4fac21fe570920bd03b2247f5
zian <mynameiszian@gmail.com>
2022-12-31 15:49:53 +0800

    added wrapper class for frontend use

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,63 @@
-import { Compiler, browserSolidityCompiler } from './browser.solidity.worker';
-import { createCompileInput } from './helpers';
+import {
+  Compiler,
+  CompilerEvent,
+  browserSolidityCompiler,
+} from './browser.solidity.worker';
+import { CompilerHelpers, createCompileInput } from './helpers';
 
+/**
+ * instantiate this as soon as possible so that the WebWoker can initialize the compiler
+ * and is ready for compilation when needed.
+ */
+export class CustomBrowserSolidityCompiler {
+  private worker: Worker;
+
+  /**
+   * instantiate this as soon as possible so that the WebWoker can initialize the compiler
+   * and is ready for compilation when needed.
+   */
+  constructor() {
+    this.worker = this.createCompilerWebWorker();
+  }
+
+  public async compile(contract: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const message = this.createCompilerInput(contract);
+
+      this.worker.postMessage(message);
+
+      this.worker.onmessage = (event) => {
+        resolve(event.data);
+      };
+
+      this.worker.onerror = (err) => {
+        reject(err);
+      };
+    });
+  }
+
+  private createCompilerWebWorker() {
+    return new Worker(
+      URL.createObjectURL(new Blob([`(new ${Compiler})`], { type: 'module' }))
+    );
+  }
+
+  private createCompilerInput(contract: string) {
+    const input = CompilerHelpers.createCompileInput(contract);
+    const event: CompilerEvent = {
+      type: 'compile',
+      compilerInput: input,
+    };
+
+    return event;
+  }
+}
+
+// --------------------------------------------------------
 const worker = new Worker(
   URL.createObjectURL(new Blob([`(new ${Compiler})`], { type: 'module' }))
 );
 
-export class CustomBrowserSolidityCompiler {}
-
-// --------------------------------------------------------
 export const solidityCompiler = async ({
   version,
   contractBody,
