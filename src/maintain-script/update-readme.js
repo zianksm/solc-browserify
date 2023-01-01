3a549a9e23661f643a3d4157dd82825999daa17d
zian <mynameiszian@gmail.com>
2023-01-01 12:31:39 +0800

    added small maintain pipline

---
+++ src/maintain-script/update-readme.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+
+async function main() {
+    const solcJsonPath = path.resolve(process.cwd(), 'node_modules', 'solc', 'package.json');
+    const solcJson = JSON.parse(fs.readFileSync(solcJsonPath, 'utf8'));
+    const version = solcJson.version;
+
+    const readmeContent = `# solc-wrapper-bundle
+using solc : ${version}
+
+\`solc/wrapper\` bundled with browserify and babelify for browser usage. intended to be used from \`WebWorker\` by importing the contents using \`importScripts()\` from [unpkg](https://unpkg.com/).`
+
+    fs.writeFileSync("./README.md", readmeContent);
+
+    return;
+}
+
+main()
