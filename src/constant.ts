e2dec3deb2a003fed284b94a5ca31f07190c8ffb
Zian <67871461+zianksm@users.noreply.github.com>
2023-01-04 17:49:17 +0800

    add support for compiler initialization callback (#6)
    * added default compiler version
    
    * remove logs
    
    * added new compiler event
    
    * added function to post message when the compiler is ready
    
    * added onready method
    
    * added compiler output event
    
    * added condition checks
    
    * update docs, and bump to 1.1.1

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,10 @@
-import { version } from './browser.solidity.worker';
+import { version } from "./browser.solidity.worker";
+
+const DEFAULT_COMPILER_VERSION = "0.8.17";
 
 /**
  * compiler version used
  */
 export const _version: version = {
-  default: '0.8.17',
+  default: DEFAULT_COMPILER_VERSION,
 } as const;
