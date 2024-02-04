6648e0c275205e2d46285c101421c1456ef0de2e
zian <mynameiszian@gmail.com>
2024-02-04 19:35:42 +0800

    feat: added langs interface

--- src/dispatch.ts
+++ src/dispatch.ts
@@ -39,3 +39,88 @@ export namespace Database {
     | RetrieveDispatch
     | StoreDispatch;
 }
+
+export namespace Compiler {
+  /**
+   * all interface are referenced from official compiler docs
+   * at https://docs.soliditylang.org/en/latest/using-the-compiler.html#compiler-api
+   */
+  export namespace Interface {
+    export type Solidity = "Solidity";
+    export const Solidity = "Solidity";
+    export type Yul = "Yul";
+    export const Yul = "Yul";
+
+    /**
+     * experimental
+     */
+    export type SolidityAst = "SolidityAst";
+    /**
+     * experimental
+     */
+    export const SolidityAst = "SolidityAst";
+
+    /**
+     * experimental
+     */
+    export type EVMAssembly = "EVMAssembly";
+    /**
+     * experimental
+     */
+    export const EVMAssembly = "EVMAssembly";
+
+    export type SupportedLanguages = Solidity | Yul | SolidityAst | EVMAssembly;
+  }
+
+  export type CallbackFunctionFragment = {
+    name: string;
+    args: string;
+    body: string;
+  };
+
+  export type CallbackResult =
+    | {
+        contents: string;
+      }
+    | {
+        error: string;
+      };
+
+  export type ImportCallback = (path: string) => CallbackResult;
+
+  export namespace Dispatchable {
+    export type Compile = "compile";
+    export const Compile = "compile";
+    export type CompilePayload = {
+      compilerInput: any;
+      importCallback?: CallbackFunctionFragment;
+    };
+    export type CompileDispatch = DispatchMessageWithPayload<
+      Compile,
+      CompilePayload
+    >;
+
+    export type Init = "init";
+    export const Init = "init";
+    export type InitPayload = {
+      // version: Version;
+    };
+    export type InitDispatch = DispatchMessageWithPayload<Init, InitPayload>;
+
+    export type Ready = "ready";
+    export const Ready = "ready";
+    export type ReadyPayload = {
+      status: boolean;
+    };
+    export type ReadyDispatch = DispatchMessageWithPayload<Ready, ReadyPayload>;
+
+    export type Out = "out";
+    export const Out = "out";
+    export type OutPayload = {
+      output: any;
+    };
+    export type OutDispatch = DispatchMessageWithPayload<Out, OutPayload>;
+  }
+
+  export namespace DispatchResult {}
+}
